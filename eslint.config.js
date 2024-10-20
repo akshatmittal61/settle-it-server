bcf0caa9f31b7432d2e82acc4c8adcfab8892c63
Akshat Mittal <akshatmittal2506@gmail.com>
2024-10-20 16:45:59 +0530

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+export default [
+	{
+		rules: {
+			"no-unused-vars": [
+				"warn",
+				{
+					args: "after-used",
+					argsIgnorePattern: "^_",
+				},
+			],
+			"no-use-before-define": "error",
+			quotes: ["error", "double"],
+			semi: ["error", "always"],
+		},
+		ignores: [
+			"/build/**",
+			"coverage/**",
+			"docs/**",
+			"!/docs/.eleventy.js",
+			"jsdoc/**",
+			"templates/**",
+			"tests/bench/**",
+			"tests/fixtures/**",
+			"tests/performance/**",
+			"tmp/**",
+			"node_modules/**",
+			"test.js",
+			"!.eslintrc.js",
+			"!.prettierrc.js",
+			"!.stylelintrc.js",
+			"!.travis.yml",
+			"!.babelrc",
+			"!.editorconfig",
+			"!.gitattributes",
+			"!.gitignore",
+			"!.npmignore",
+			"!.nvmrc",
+			"!.vscode/**",
+			"!.next/**",
+		],
+	},
+];
