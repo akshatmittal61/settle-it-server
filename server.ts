ba7bdbb026c24b7058b01c77076e56b101bc7a1c
Akshat Mittal <akshatmittal2506@gmail.com>
2024-10-20 17:11:51 +0530

    reconfigured project

--- server.ts
+++ server.ts
@@ -1,4 +1,15 @@
-import express from 'express'
+import express, { Request, Response } from "express";
+import { PORT } from "./config";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/health", (_: Request, res: Response) => {
+	return res.status(200).json({ message: "API is healthy" });
+});
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
