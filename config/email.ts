9a222fe851d922d1e6e240a47bb92bbaeca28950
Akshat Mittal <akshatmittal2506@gmail.com>
2024-12-25 01:39:36 +0530

    fix: mailing client

--- config/email.ts
+++ config/email.ts
@@ -2,9 +2,6 @@ import { GOOGLE_MAIL_SERVICE_KEYS } from "../types";
 import { configService } from "./base";
 
 export const googleEmailConfig: Record<GOOGLE_MAIL_SERVICE_KEYS, string> = {
-	clientId: configService.get("GOOGLE_CLIENT_ID"),
-	clientSecret: configService.get("GOOGLE_CLIENT_SECRET"),
-	refreshToken: configService.get("GOOGLE_REFRESH_TOKEN"),
-	redirectUri: configService.get("GOOGLE_REDIRECT_URI"),
 	email: configService.get("GOOGLE_EMAIL"),
+	password: configService.get("GOOGLE_EMAIL_PASSWORD"),
 };
--- types/enum.ts
+++ types/enum.ts
@@ -4,12 +4,7 @@ export type T_OTP_STATUS = "PENDING" | "EXPIRED";
 export type T_EXPENSE_STATUS = "PENDING" | "SETTLED";
 export type T_NODE_ENV = "development" | "test" | "production";
 
-export type GOOGLE_MAIL_SERVICE_KEYS =
-	| "clientId"
-	| "clientSecret"
-	| "refreshToken"
-	| "redirectUri"
-	| "email";
+export type GOOGLE_MAIL_SERVICE_KEYS = "email" | "password";
 
 export type LOG_LEVEL =
 	| "log"
